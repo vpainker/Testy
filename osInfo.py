63d0804e2c2826f8afdf31573efe82155822ac9e
vpainker <vlpainker@gmail.com>
2022-05-15 19:17:25 +0300

    Определил расстояние от этой точки до начала координат Аналогично для другой точки

---
+++ osInfo.py
@@ -0,0 +1,10 @@
+import platform
+import sys
+
+
+info = 'OS info is \n{} \n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('osInfo.txt', 'w') as ff:
+    ff.write(info)
